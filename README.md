eb6f2183f51b3ca1e2884cc899851f7353fab935
KhushYadav1909 <55939687+KhushYadav1909@users.noreply.github.com>
2019-09-29 12:12:36 +0530

    Update and rename README.md to Swapping.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Repo1777
-Basic code in Python
---
+++ Swapping.py
@@ -0,0 +1,10 @@
+# Repo1777
+# Basic code in Python
+x=int(input("Enter 1st No."))
+print("Entered 1st No.",x)
+y=int(input("Enter 2nd No."))
+print("Entered 2nd N0.",y)
+temp=x
+x=y
+y=temp
+print("The No. After Swapping Are",x,y)
